23ed91749955199cd0012759abf32b0f88292e67
Oleksii Bielichenko <byelichenko@gmail.com>
2023-03-09 23:03:35 +0200

    done

--- task.py
+++ task.py
@@ -10,53 +10,54 @@ store = {
     'sprite': 42,
 }
 
+is_valid = None
 
 user_name = input('Enter your name: ')
-# user_email = input('Enter your email: ')
-# user_password = input('Enter your password: ')
-# is_valid = None
+user_email = input('Enter your email: ')
+user_password = input('Enter your password: ')
 
 
-# def name_validation():
-#     global user_name
-#     if len(user_name) < 2:
-#         return False
-#     else:
-#         return True
+def name_validation():
+    global user_name
+    if len(user_name) < 2:
+        return False
+    else:
+        return True
 
 
-# def email_validation():
-#     global user_email
-#     if '@' in user_email and '.' in user_email:
-#         return True
-#     else:
-#         return False
+def email_validation():
+    global user_email
+    if '@' in user_email and '.' in user_email:
+        return True
+    else:
+        return False
 
 
-# def password_validation():
-#     global user_password
-#     if len(user_password) > 6:
-#         return True
-#     else:
-#         return False
+def password_validation():
+    global user_password
+    if len(user_password) > 6:
+        return True
+    else:
+        return False
 
 
-# def registration():
-#     global is_valid
+def registration():
+    global is_valid
 
-#     is_valid_name = name_validation()
-#     is_valid_email = email_validation()
-#     is_valid_password = password_validation()
+    is_valid_name = name_validation()
+    is_valid_email = email_validation()
+    is_valid_password = password_validation()
 
-#     if is_valid_name and is_valid_email and is_valid_password:
-#         is_valid = True
-#         print('It is valid data')
-#     else:
-#         is_valid = False
-#         print('It is invalid data')
+    if is_valid_name and is_valid_email and is_valid_password:
+        is_valid = True
+        print('It is valid data')
+    else:
+        is_valid = False
+        print('It is invalid data')
+        exit()
 
 
-# registration()
+registration()
 
 
 cash = int(
@@ -71,9 +72,13 @@ else:
     print("Our goods: ", store)
     print(f"What do you want to buy, {user_name.capitalize()}? ")
 
-while cash >= 0:
+while cash > 0:
     order = input("Enter product name: ")
 
     if order in store:
         cash = cash - store[order]
-        print(cash)
+        print(f"You have {cash} left")
+    else:
+        print(f"Sorry, {user_name.capitalize()}, "
+              f"but we don't have the {order}")
+print(f"Good bye, {user_name.capitalize()}!")
